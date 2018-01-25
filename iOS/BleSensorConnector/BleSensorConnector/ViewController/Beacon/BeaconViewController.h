65f4b6576d7eabe3f52eccf25070880b51a9891c
Mark Chen <chenjian345@gmail.com>
2018-01-25 15:44:24 +0800

    Add Ble beacon debug view controller

---
+++ iOS/BleSensorConnector/BleSensorConnector/ViewController/Beacon/BeaconViewController.h
@@ -0,0 +1,13 @@
+//
+//  BeaconViewController.h
+//  BleSensorConnector
+//
+//  Created by Mark on 2018/1/25.
+//  Copyright © 2018年 MarkCJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeaconViewController : UIViewController
+
+@end
